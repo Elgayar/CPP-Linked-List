3fb1205200161c09c6efe0df15b0e2abf9d3dbdf
Mostafa Elgayar <meeger99@gmail.com>
2017-12-02 00:33:13 +0200

    Removed Unnecessary Comments

--- main.cpp
+++ main.cpp
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   main.cpp
  * Author: Mostafa Elgayar
@@ -13,12 +7,8 @@
 
 #include <cstdlib>
 #include "List.h"
-
 using namespace std;
 
-/*
- * 
- */
 int main(int argc, char** argv) {
     List Arabella;
 
@@ -33,4 +23,3 @@ int main(int argc, char** argv) {
 
     return 0;
 }
-
